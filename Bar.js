6a0d474662686dfa7ca9088d91c1a63344531f9e
Joel Arvidsson <joel@oblador.se>
2015-10-31 18:20:06 -0400

    Adds indeterminate state to Progress.Bar.

--- Bar.js
+++ Bar.js
@@ -3,9 +3,12 @@
 var React = require('react-native');
 var {
   Animated,
+  Easing,
   View,
   PropTypes,
 } = React
+
+var INDETERMINATE_WIDTH_FACTOR = 0.3;
 
 var ProgressBar = React.createClass({
   propTypes: {
@@ -18,6 +21,7 @@ var ProgressBar = React.createClass({
     color: PropTypes.string,
     borderColor: PropTypes.string,
     unfilledColor: PropTypes.string,
+    indeterminate: PropTypes.bool,
   },
 
   getDefaultProps: function() {
@@ -29,31 +33,57 @@ var ProgressBar = React.createClass({
       borderWidth: 1,
       borderRadius: 4,
       color: 'rgba(0, 122, 255, 1)',
+      indeterminate: false,
     };
   },
 
   getInitialState: function() {
     var progress = Math.min(Math.max(this.props.progress, 0), 1);
-    var animationValue = new Animated.Value(progress);
     return {
-      animationValue,
+      progress: new Animated.Value(progress),
+      animationValue: new Animated.Value(0),
     };
   },
 
+  componentDidMount: function() {
+    if(this.props.indeterminate) {
+      this.animate();
+    }
+  },
+
   componentWillReceiveProps: function(props) {
-    if(props.progress === this.props.progress) {
-      return;
+    if(props.indeterminate !== this.props.indeterminate) {
+      if(props.indeterminate) {
+        this.animate();
+      } else {
+        this.state.animationValue.setValue(0);
+      }
     }
+    if(props.progress !== this.props.progress) {
+      var progress = Math.min(Math.max(props.progress, 0), 1);
 
-    var progress = Math.min(Math.max(props.progress, 0), 1);
-
-    if(this.props.animated) {
-      Animated.spring(this.state.animationValue, {
-        toValue: progress,
-      }).start();
-    } else {
-      this.state.animationValue.setValue(progress);
+      if(this.props.animated) {
+        Animated.spring(this.state.progress, {
+          toValue: progress,
+        }).start();
+      } else {
+        this.state.progress.setValue(progress);
+      }
     }
+  },
+
+  animate: function() {
+    this.state.animationValue.setValue(0);
+    Animated.timing(this.state.animationValue, {
+      toValue: 1,
+      duration: 1000,
+      easing: Easing.linear,
+      isInteraction: false,
+    }).start(endState => {
+      if(endState.finished) {
+        this.animate();
+      }
+    });
   },
 
   render: function() {
@@ -66,6 +96,7 @@ var ProgressBar = React.createClass({
       color,
       borderColor,
       unfilledColor,
+      indeterminate,
       children,
       ...props
     } = this.props;
@@ -78,18 +109,28 @@ var ProgressBar = React.createClass({
       overflow: 'hidden',
       backgroundColor: unfilledColor,
     };
-    var progresStyle = {
+    var progressStyle = {
       backgroundColor: color,
       height,
-      width: this.state.animationValue.interpolate({
+    };
+    if(indeterminate) {
+      progressStyle.width = width * INDETERMINATE_WIDTH_FACTOR;
+      progressStyle.transform = [{
+        translateX: this.state.animationValue.interpolate({
+          inputRange: [0, 1],
+          outputRange: [width * -INDETERMINATE_WIDTH_FACTOR, width]
+        })
+      }]
+    } else {
+      progressStyle.width = this.state.progress.interpolate({
         inputRange: [0, 1],
         outputRange: [0, width],
-      }),
-    };
+      });
+    }
 
     return (
       <View style={[containerStyle, style]} {...props}>
-        <Animated.View style={progresStyle} />
+        <Animated.View style={progressStyle} />
         {children}
       </View>
     );
