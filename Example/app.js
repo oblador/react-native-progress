42c219e797d51d3ff3f7e8f0e55db00eb8e8b0d7
Joel Arvidsson <joel@oblador.se>
2016-05-19 12:26:20 +0200

    Upgrades example to RN 0.26 and ES2016.

---
+++ Example/app.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import * as Progress from 'react-native-progress';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  circles: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  progress: {
+    margin: 10,
+  },
+});
+
+export default class Example extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      progress: 0,
+      indeterminate: true,
+    };
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    let progress = 0;
+    this.setState({ progress });
+    setTimeout(() => {
+      this.setState({ indeterminate: false });
+      setInterval(() => {
+        progress += Math.random() / 5;
+        if (progress > 1) {
+          progress = 1;
+        }
+        this.setState({ progress });
+      }, 500);
+    }, 1500);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Progress Example</Text>
+        <Progress.Bar
+          style={styles.progress}
+          progress={this.state.progress}
+          indeterminate={this.state.indeterminate}
+        />
+        <View style={styles.circles}>
+          <Progress.Circle
+            style={styles.progress}
+            progress={this.state.progress}
+            indeterminate={this.state.indeterminate}
+          />
+          <Progress.Pie
+            style={styles.progress}
+            progress={this.state.progress}
+            indeterminate={this.state.indeterminate}
+          />
+          <Progress.Circle
+            style={styles.progress}
+            progress={this.state.progress}
+            indeterminate={this.state.indeterminate}
+            direction="counter-clockwise"
+          />
+        </View>
+        <View style={styles.circles}>
+          <Progress.CircleSnail
+            style={styles.progress}
+          />
+        <Progress.CircleSnail
+            style={styles.progress}
+            color={[
+              '#F44336',
+              '#2196F3',
+              '#009688',
+            ]}
+          />
+        </View>
+      </View>
+    );
+  }
+}
