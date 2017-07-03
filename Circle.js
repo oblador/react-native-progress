379152729f420a9b58e8f695b5a6f211fff5107c
Mike Hobizal <mikehobizal@gmail.com>
2017-07-03 14:42:59 -0700

    Add `strokeCap` prop for `Progress.Circle` (#45)
    * Add `strokeCap` prop for `Progress.Circle`
    
    Changes: Allows for further customization of the `Progress.Circle` by adding a `strokeCap` prop, similar to the `Progress.CircleSnail` component.
    
    * Add prop to Arc to allow for custom strokeCap
    
    * Update project readme

--- Circle.js
+++ Circle.js
@@ -89,6 +89,7 @@ export class ProgressCircle extends Component {
       showsText,
       size,
       style,
+      strokeCap,
       textStyle,
       thickness,
       unfilledColor,
@@ -154,6 +155,7 @@ export class ProgressCircle extends Component {
               startAngle={0}
               endAngle={(indeterminate ? 1.8 : 2) * Math.PI}
               stroke={borderColor || color}
+              strokeCap={strokeCap}
               strokeWidth={border}
             />
           ) : false}
