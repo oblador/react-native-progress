86cccad1785c48879b499e798f7e7b165e43b017
Joel Arvidsson <joel@oblador.se>
2019-03-21 17:50:08 +0100

    Rename indeterminateAnimationDuration again

--- withAnimation.js
+++ withAnimation.js
@@ -12,13 +12,13 @@ export default function withAnimation(WrappedComponent, indeterminateProgress) {
       animated: PropTypes.bool,
       direction: PropTypes.oneOf(['clockwise', 'counter-clockwise']),
       indeterminate: PropTypes.bool,
-      indeterminateMoveSpeed: PropTypes.number,
+      indeterminateAnimationDuration: PropTypes.number,
       progress: PropTypes.number.isRequired,
     };
 
     static defaultProps = {
       animated: true,
-      indeterminateMoveSpeed: 1000,
+      indeterminateAnimationDuration: 1000,
       indeterminate: false,
       progress: 0,
     };
@@ -89,7 +89,7 @@ export default function withAnimation(WrappedComponent, indeterminateProgress) {
       this.state.rotation.setValue(0);
       Animated.timing(this.state.rotation, {
         toValue: this.props.direction === 'counter-clockwise' ? -1 : 1,
-        duration: this.props.indeterminateMoveSpeed,
+        duration: this.props.indeterminateAnimationDuration,
         easing: Easing.linear,
         isInteraction: false,
       }).start(endState => {
