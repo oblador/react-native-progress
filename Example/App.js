8206718fa33415b8390b72871c05c1f6c6a27b8d
Joel Arvidsson <joel@oblador.se>
2021-06-30 16:25:30 +0200

    Bump example to RN 0.64 (#230)

--- Example/App.js
+++ Example/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import { StyleSheet, Text, View } from 'react-native';
-
 import * as Progress from 'react-native-progress';
 
 const styles = StyleSheet.create({
